ab7c0581b1c21a93a56d3ac9a425d09395655b43
Đinh Phương Nam <65706866+tian073@users.noreply.github.com>
2021-04-26 17:29:07 +0700

    upload2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
+// Menu dropdown
 let trans = document.querySelector('.transparent');
 let menu = document.querySelector('.menu');
 var menuDropdown = document.querySelector('.menu-dropdown');
 let menuDropdownItem = document.querySelector('.menu-dropdown ul');
+// Nav mobile
 let navMobile = document.querySelector('.nav-mobile');
 let overlay = document.querySelector('.overlay');
 let iconClose = document.querySelector('.close-icon');
-
-
+// Owl selection
+let box = document.querySelector('.product-details-content .details-left');
+let btnPre = document.querySelector('.selection .round-left');
+let btnNext = document.querySelector('.selection .round-right');
+let owl = document.querySelector('.owl');
 
 // Menu dropdown
 menu.onclick = () => {
@@ -62,6 +67,59 @@ navMobile.onclick = () => {
 iconClose.onclick = () => {
     overlay.style.width = '0%';
 }
+
+// Own selection
+let elmBody = document.body;
+if(elmBody.contains(btnPre) && elmBody.contains(btnNext) && elmBody.contains(owl)) {
+    btnPre.onclick = () => {
+        // remove all text nodes
+        owl.childNodes.forEach(elm => {
+            elm.nodeType != 1 && elm.parentNode.removeChild(elm);
+        })
+    
+        owl.appendChild(owl.firstChild);
+    }
+    
+    btnNext.onclick = () => {
+        // remove all text nodes
+        owl.childNodes.forEach(elm => {
+            elm.nodeType != 1 && elm.parentNode.removeChild(elm);
+        })
+    
+        owl.insertBefore(owl.lastChild, owl.firstChild)
+    }
+    
+    for(x of owl.children) {
+        x.onmouseover = (e) => {
+            var coppy = e.target.cloneNode(true);
+            // coppy.style.transform = 'scale(1.2)'
+            console.log(box.firstChild.nextSibling);
+            box.replaceChild(coppy, box.firstChild.nextSibling)
+        }
+    
+        x.onmouseleave = (e) => {
+            var coppy = e.target.cloneNode(true);
+            // coppy.style.transform = 'scale(1)';
+            box.replaceChild(coppy, box.firstChild.nextSibling)
+        }
+    }
+}
+
+// See more
+$("#btn-slide-down-1").click(function(){
+    $("#btn-slide-down-1").css("display","none");
+    $("#slide-down-1").slideDown("slow");
+});
+
+$("#btn-slide-down-2").click(function(){
+    $("#btn-slide-down-2").css("display","none");
+    $("#slide-down-2").slideDown("slow");
+});
+
+$("#btn-slide-down-3").click(function(){
+    $("#btn-slide-down-3").css("display","none");
+    $("#slide-down-3").slideDown("slow");
+});
 
 
 
